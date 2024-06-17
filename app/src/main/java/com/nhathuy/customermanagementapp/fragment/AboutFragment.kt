4b24dc8f5bc0d660e9f460dd9148c37cd5c2e8c0
TravisHuy <honhathuy098@gmail.com>
2024-06-17 12:58:10 +0700

    add aboutfragment

--- app/src/main/java/com/nhathuy/customermanagementapp/fragment/AboutFragment.kt
+++ app/src/main/java/com/nhathuy/customermanagementapp/fragment/AboutFragment.kt
@@ -1,25 +1,44 @@
 package com.nhathuy.customermanagementapp.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nhathuy.customermanagementapp.R
+import com.nhathuy.customermanagementapp.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
-
+    private lateinit var binding: FragmentAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding= FragmentAboutBinding.inflate(layoutInflater)
+
+
+        binding.facebook.setOnClickListener{
+            openUrl("https://www.facebook.com/honhathuy.travishuy")
+        }
+        binding.github.setOnClickListener{
+            openUrl("https://github.com/TravisHuy")
+        }
+        binding.linkedin.setOnClickListener{
+            openUrl("https://www.linkedin.com/in/honhathuy/")
+        }
+        return binding.root
+    }
+
+    private fun openUrl(url: String) {
+        val intent=Intent(Intent.ACTION_VIEW)
+        intent.data=Uri.parse(url)
+        startActivity(intent)
     }
 
 }
