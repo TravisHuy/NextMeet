5a755e53866fd69c32cba7b0897369e42769ff58
TravisHuy <honhathuy098@gmail.com>
2025-05-24 08:59:53 +0700

    update test activity

---
+++ app/src/main/java/com/nhathuy/nextmeet/ui/TestActivity.kt
@@ -0,0 +1,13 @@
+
+package com.nhathuy.nextmeet.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nhathuy.nextmeet.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
